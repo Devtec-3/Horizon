c779b3a61b8ba7eb89453575048cae67846557e3
Devtec <muhammadabdulwadudalata@gmail.com>
2026-07-23 06:55:09 +0100

    Update index.ts

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -1,5 +1,7 @@
 // Export your models here. Add one export per file
 // export * from "./posts";
+export * from "./users";
+
 //
 // Each model/table should ideally be split into different files.
 // Each model/table should define a Drizzle table, insert schema, and types:
